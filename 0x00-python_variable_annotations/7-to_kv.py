9e77ddb2481d166279d4ea21e29e5932263cf982
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-06 21:09:55 +0100

    it was pep8 all along

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Write a type-annotated function to_kv that takes
  a string k and an int OR float v as arguments and returns a tuple.
- The first element of the tuple is the string k. The second element is the square of the
+ The first element of the tuple is the string k.
+ The second element is the square of the
  int/float v and should be annotated as a float.
  """
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-	"""
-	Put the arguments in a tuple
-	:param k: string argument
-	:param v: second argument
-	:return: tuple
-	"""
-	out_tuple: Tuple[str, float] = (k, v*v)
-	return out_tuple
+    """
+    Put the arguments in a tuple
+    :param k: string argument
+    :param v: second argument
+    :return: tuple
+    """
+    return (k, v*v)
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-"""Annotate the below function’s parameters and return values with the appropriate types"""
+"""Annotate the below function’s parameters and return values
+with the appropriate types
+"""
 from typing import Iterable, Sequence, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-	"""
-	Gives one a modified list which has a tuple for each element in 'lst'.
-	this tuple contains the element and the first entry followed by the length of the element.
-	:param lst: list to be counter
-	:return: list of tuple.
-	"""
-	return [(i, len(i)) for i in lst]
+    """
+    Gives one a modified list which has a tuple for each element in 'lst'.
+    this tuple contains the element and the first entry followed
+    by the length of the element.
+    :param lst: list to be counter
+    :return: list of tuple.
+    """
+    return [(i, len(i)) for i in lst]
