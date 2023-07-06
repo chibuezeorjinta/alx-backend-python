80b11b1ee090dca93172e5a8eabc84476c51a732
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-06 21:51:09 +0100

    make typeVar equal to the type value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,11 +5,11 @@ Hint: look into TypeVar.
 Turns out TypeVar is like in interface for Typescript"""
 from typing import Mapping, Any, Union, TypeVar
 
-X = TypeVar('X')
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[X, None]) -> Union[Any, X]:
+                     default: Union[T, None]) -> Union[Any, T]:
     """
     Check if key is in dct. if not return default which is
     either new type 'X' or none.
